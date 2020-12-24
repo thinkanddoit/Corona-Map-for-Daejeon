5d6ad01b583fc8985b9357d4c3e444c51e20ac1d
easyone <thinkanddoit@naver.com>
2020-12-24 14:13:46 +0900

    Clone the opensource component code - https://github.com/bearsjelly/kakaomapForBlog 사이트 kako Map 코드를   복사해서 우선 지도상 마커를 찍음

--- src/App.js
+++ src/App.js
@@ -1,76 +1,12 @@
 import React, { useState, useEffect } from 'react'
-
-const APP_KEY = '5009e1aa3eac24e829865f6471ae1d56'
-const divBtnOpt = {
-  width: '50px',
-  height: '50px',
-  position: 'fixed',
-  top: '100px',
-  zIndex: '10',
-}
+import Map from './Map'
 
 const App = () => {
-  const [map, setMap] = useState(null)
-  const [markerArr, setMarkerArr] = useState([])
-  const [locationArr, setLocationArr] = useState([])
-
-  const getLocation = async id => {
-    const data = await fetch(`http://localhost:3000/data${id}.json`)
-    console.log(data)
-    const dataJSON = await data.json()
-    setLocationArr(dataJSON.location)]
-  }
-
-  const createMap = () => {
-    const script = document.createElement('script')
-    script.async = true
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${APP_KEY}&autoload=false`
-    document.head.appendChild(script)
-    script.onload = () => {
-      const { kakao } = window
-      kakao.maps.load(() => {
-        let container = document.getElementById('Mymap')
-        let options = {
-          center: new kakao.maps.LatLng(36.350473962094654, 127.3848508837152),
-          level: 8,
-        }
-        const createdMap = new kakao.maps.Map(container, options)
-        setMap(createdMap)
-      })
-    }
-  }
-
-  const createMarker = () => {
-    const { kakao } = window
-    const tempArr = []
-    locationArr.forEach(e => {
-      tempArr.push(
-        new kakao.maps.Marker({
-          map: map,
-          position: new kakao.maps.LatLng(36.350473962094654, 127.3848508837152),
-        })
-      )
-    })
-    setMarkerArr(tempArr)
-  }
-
-  const deleteMarker = () => markerArr.forEach(e => e.setMap(null))
-
-  useEffect(() => {
-    getLocation(1) // location 정보 fetch
-    createMap()
-  }, [])
-
-  // marker 생성 + 표시
-  useEffect(() => map && locationArr.length && createMarker(), [
-    map,
-    locationArr,
-    
-  ])
+  
 
   return (
     <div className="App">
-      <div id="Mymap" style={{ width: '100vw', height: '100vh' }}></div>
+      <Map />
     </div>
 
   )
---
+++ src/Map.js
@@ -0,0 +1,85 @@
+//5009e1aa3eac24e829865f6471ae1d56
+
+/*global kakao */
+import React, { useEffect } from "react";
+const markerdata = [
+  {
+    title: "콜드스퀘어",
+    lat: 37.62197524055062,
+    lng: 127.16017523675508,
+  },
+  {
+    title: "하남돼지집",
+    lat: 37.620842424005616,
+    lng: 127.1583774403176,
+  },
+  {
+    title: "수유리우동",
+    lat: 37.624915253753194,
+    lng: 127.15122688059974,
+  },
+  {
+    title: "맛닭꼬",
+    lat: 37.62456273069659,
+    lng: 127.15211256646381,
+  },
+];
+
+export default function Map() {
+  useEffect(() => {
+    mapscript();
+  }, []);
+
+  const mapscript = () => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+      level: 8,
+    };
+
+    //map
+    const map = new kakao.maps.Map(container, options);
+    markerdata.forEach((el) => {
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        //마커가 표시 될 지도
+        map: map,
+        //마커가 표시 될 위치
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+      });
+      // 마커에 표시할 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content: el.title, // 인포윈도우에 표시할 내용
+      });
+
+      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+      // 이벤트 리스너로는 클로저를 만들어 등록합니다
+      // 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
+    });
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+    function makeOverListener(map, marker, infowindow) {
+      return function () {
+        infowindow.open(map, marker);
+      };
+    }
+
+    // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+    function makeOutListener(infowindow) {
+      return function () {
+        infowindow.close();
+      };
+    }
+  };
+
+  return <div id="map" style={{ width: "100vw", height: "100vh" }}></div>;
+}
